56bfe3ebb9b098d430135c5d6c864a1db670d7a7
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-23 11:15:31 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//android_id
+$anid="xxxxxxxxxxxxxxx";
+//data sign
+$sign="xxxxxxxxxxxxxxx";
+//data aid jika tidak ada kosongkan
+$aid="xxxxxxxxxxxxxxx";
+//data uuid
+$uuid="xxxxxxxxxxxxxxx";
+?>
